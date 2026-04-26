26495bca0700d4b76cca6757a268d68ed7d9eebd
roussalanoca-cmd <rouss.alanoca@utec.edu.pe>
2026-04-26 14:59:38 -0500

    Update hola_mundo.js

--- hola_mundo.js
+++ hola_mundo.js
@@ -1,21 +1,11 @@
-// archivo: index.js
+const app = require("express")();
 
-// 1. Importar express
-const express = require("express");
-const app = express();
+const PORT = process.env.PORT || 3000;
 
-// 2. Función auxiliar
-function obtenerMensaje() {
-  return "¡Hola Mundo desde Node.js en la nube!";
-}
-
-// 3. Ruta principal
 app.get("/", (req, res) => {
-  res.send(obtenerMensaje());
+  res.send("Hola mundo");
 });
 
-// 4. Iniciar servidor
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Servidor escuchando en puerto ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
