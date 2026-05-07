e53e517506763c942cbd936320308b828a7ff9bc
roussalanoca-cmd <rouss.alanoca@utec.edu.pe>
2026-05-07 16:19:57 -0500

    Update hola_mundo.js

--- hola_mundo.js
+++ hola_mundo.js
@@ -2,12 +2,19 @@ const express = require("express");
 const app = express();
 
 function obtenerMensje() {
-return "¡Hola mundo desde Node.js en la nube!;
+  return "¡Hola mundo desde Node.js en la nube!";
 }
 
+function obtenerHora() {
+  const ahora = new Date (); // Date()
+  return ahora_tolocalString("es-PE", ( timeZone: "America/Lima"));
+} 
+  
 app.get("/", (req, res) => {
-  res.send(obtener mensaje());
+  res.send(´$(obtener mensaje())<br>Hora actual: $(obtenerHora()}´);
 });
+
+app.get("/health",(req, res) => res.status(200).send("ok"));
 
 conat PORT = process.env.PORT || 5000;
 app.listen(PORT, "0.0.0.0, () => {
